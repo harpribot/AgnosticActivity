0ff2ed9140220d6c5a4267d07e9c83b521a2e224
Tushar <tushar.nagarajan@gmail.com>
2016-11-21 20:20:13 -0600

    PR curve scripts

--- Experiments/Evaluation_Plots/parse_results.py
+++ Experiments/Evaluation_Plots/parse_results.py
@@ -4,6 +4,8 @@ import numpy as np
 
 def parse_preds(pred_dict, out_fl):
 	
+	out_fl=out_fl.split('/')[-1].split('.pkl')[0]+'.curve'
+
 	preds=sorted(pred_dict.items(), key=lambda x: x[0])
 	files, preds= zip(*preds)
 	truth, preds= zip(*preds)
@@ -23,29 +25,42 @@ def parse_preds(pred_dict, out_fl):
 		
 	prec_at_k/=pred_idx.shape[0]
 	
-	out_fl=out_fl.split('/')[-1].split('.pkl')[0]+'.curve'
-	out_fl='curves/P@k/'+out_fl
-
-	with open(out_fl, 'w') as f:
+	with open('curves/P@k/'+out_fl, 'w') as f:
 		for k in range(prec_at_k.shape[0]):
 			f.write('%d\t%f\n'%(k, prec_at_k[k]))
 
 
-	'''
 	# code to generate PR curve values
-	
-	sorted_pred=sorted(zip(preds, truth), key=lambda x: -x[0])
 
+	# set a threshold for counting positives (only for computation)
+	# ideally, plot all of them - the low ones will fall at the tail
+	# of the curve anyway
+
+	thresh=0.1
+	truth_stack=np.vstack([truth]*preds.shape[1]).transpose()
+	idx_stack=np.vstack([np.arange(preds.shape[1])]*preds.shape[0])
+	
+	keep=(preds>thresh)
+	
+	flat_preds=zip(preds[keep], idx_stack[keep], truth_stack[keep])
+
+	# sort according to the most confident predictions
+	sorted_pred=sorted(flat_preds, key=lambda x: -x[0])
 
 	pr_data=[]
-	correct, predicted=0, 0
-	for b_no, p_idx, p_val in sorted_pred:
+	correct, predicted, total=0, 0, preds.shape[0]
+	for p_val, p_idx, lab in sorted_pred:
 		predicted+=1
-		if p_idx in block_gold[b_no]:
+		if p_idx==lab:
 			correct+=1
 		pr_data.append((1.0*correct/predicted, 1.0*correct/total))
-	'''
 
+	print len(pr_data)
+
+	with open('curves/PR/'+out_fl, 'w') as f:
+		for P, R in pr_data:
+			f.write('%f\t%f\n'%(P,R))
+	
 
 
 pred_files=glob.glob('../Prediction_Pickles/*.pkl')
--- Experiments/Evaluation_Plots/plot_curves.py
+++ Experiments/Evaluation_Plots/plot_curves.py
@@ -7,15 +7,46 @@ import sys
 import numpy as np
 
 
-for curve_fl in sys.argv[1:]:
-	with open(curve_fl,'r') as f:
-		data=f.read().strip().split('\n')
-	data=[map(float, d.split('\t')) for d in data]
-	data=np.array(data)
+def plotPR(curve_files, out_file):
+	for curve_fl in curve_files:
+		with open(curve_fl,'r') as f:
+			data=f.read().strip().split('\n')
+		data=[map(float, d.split('\t')) for d in data]
+		prec, rec=zip(*data)
 
-	curve_name=curve_fl.split('/')[-1]
-	plt.plot(data[:,0], data[:,1], label=curve_name)
+		curve_name=curve_fl.split('/')[-1]
+		plt.plot(rec, prec, label=curve_name)
+
+	plt.xlabel('recall')
+	plt.ylabel('precision')
+
+	plt.legend()
+	plt.savefig(out_file)
+	plt.clf()
+
+def plotP_at_K(curve_files, out_file):
+	for curve_fl in curve_files:
+		with open(curve_fl,'r') as f:
+			data=f.read().strip().split('\n')
+		data=[map(float, d.split('\t')) for d in data]
+		data=np.array(data)
+
+		curve_name=curve_fl.split('/')[-1]
+		plt.plot(data[:,0], data[:,1], label=curve_name)	
+
+	plt.xlabel('K')
+	plt.ylabel('precision@K')
+
+	plt.legend()
+	plt.savefig(out_file)
+	plt.clf()
 
 
-plt.legend()
-plt.savefig('P@k.png')
+plotP_at_K(glob.glob('curves/P@k/*subset.curve'), 'P@K_subset.png')
+plotP_at_K(glob.glob('curves/P@k/*val.curve'), 'P@K_imval.png')
+plotP_at_K(glob.glob('curves/P@k/*test.curve'), 'P@K_imtest.png')
+	
+plotPR(glob.glob('curves/PR/*subset.curve'), 'PR_subset.png')
+plotPR(glob.glob('curves/PR/*val.curve'), 'PR_imval.png')
+plotPR(glob.glob('curves/PR/*test.curve'), 'PR_imtest.png')
+
